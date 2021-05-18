615c484726ed80becaf562f5818e4e691ce01a8c
lequang-hp <lequang0810@gmail.com.vn>
2021-05-18 01:55:05 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCZqDkr0c_UQG5zA6Pl0RNRbiT51RX0K8",
+  authDomain: "fir-sample-5087b.firebaseapp.com",
+  projectId: "fir-sample-5087b",
+  storageBucket: "fir-sample-5087b.appspot.com",
+  messagingSenderId: "956224278673",
+  appId: "1:956224278673:web:1cde9c25469d74f1152f30"
+};
+firebase.initializeApp(firebaseConfig);
